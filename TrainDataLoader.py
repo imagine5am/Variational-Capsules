882721cbc96009c2500b435a0a5990bcb94e9a1b
SHIVAM SOOD <ssood@cse.iitb.ac.in>
2020-11-22 17:36:37 +0530

    ICDAR data

--- TrainDataLoader.py
+++ TrainDataLoader.py
@@ -4,10 +4,54 @@ import h5py
 import numpy as np
 import os
 import random
+import skvideo.io 
+import xml.etree.ElementTree as ET
 
 from PIL import Image, ImageDraw
 from scipy.misc import imread
 from tqdm import tqdm
+
+out_h, out_w = 480, 480
+
+def save_masked_video(name, video, mask):
+    alpha = 0.5
+    color = np.zeros((3,)) + [0.0, 0, 1.0]
+    masked_vid = np.where(np.tile(mask, [1, 1, 3]) == 1, video * (1 - alpha) + alpha * color, video)
+    skvideo.io.vwrite(name+'_segmented.avi', (masked_vid * 255).astype(np.uint8))
+
+
+def order_points(pts):
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype = "int32")
+
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # return the ordered coordinates
+    return rect.flatten().tolist()
+
+def resize_and_pad(shape, im):
+    in_h, in_w = shape[0], shape[1]
+    if out_w / out_h > in_w / in_h:
+        h, w = out_h, in_w * out_h // in_h
+    elif out_w / out_h < in_w / in_h:
+        h, w =  in_h * out_w // in_w, out_w
+
+    im = cv2.resize(im, (w, h))
+    delta_w = out_w - w
+    delta_h = out_h - h
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+    
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT)
+    return im
 
 
 def get_det_annotations(ann_file, split='train'):
@@ -61,11 +105,44 @@ def create_mask(shape, pts):
     # input()
     return np.asarray(im)
 
+
+def list_vids(dir):
+    allfiles = os.listdir(dir)
+    files = [ fname for fname in allfiles if fname.endswith('.mp4')]
+    return files
+
+
+def parse_ann(file):
+    '''
+    Returns a dict which is something like:
+    {frame_num:{object_id: polygon_pts_list, ...}, ...}
+    '''
+    tree = ET.parse(file)
+    root = tree.getroot()
+    ann = {}
+    for frame in root.findall('./frame'):
+        frame_num = int(frame.attrib['ID']) - 1
+        objects = {}
+        for object in frame.findall('./object'):
+            id = int(object.attrib['ID'])
+            pts = []
+            for pt in object.findall('./Point'):
+                pts.append((int(pt.attrib['x']), int(pt.attrib['y'])))
+            objects[id] = order_points(np.array(pts))
+        ann[frame_num] = objects
+    return ann
+
+
 class TrainDataLoader:
     def __init__(self, total_images = 10000):
         self.synth_data_loc = '/mnt/data/Rohit/VideoCapsNet/code/SynthVideo/out'
-        synth_data = self.load_synth_data(7000) 
-        print(len(synth_data))
+        #synth_data = self.load_synth_data(7000) 
+        icdar_data = self.load_icdar_data()
+        #print(len(synth_data))
+        print(len(icdar_data))
+        print(icdar_data[0][0].shape)
+        print(icdar_data[0][1].shape)
+        
 
     def load_synth_data(self, num_images):
         ann_file = os.path.join(self.synth_data_loc, 'Annotations', 'synthvid_ann.hdf5')
@@ -97,7 +174,41 @@ class TrainDataLoader:
                 data.append((frame, mask, 'synth'))
         
         return data
+    
+    
+    def load_icdar_data(self, split_type='train'):
+        if split_type=='train':
+            icdar_loc = '/mnt/data/Rohit/ICDARVideoDataset/text_in_Video/ch3_train/'
+        elif split_type=='test':
+            icdar_loc = '/mnt/data/Rohit/ICDARVideoDataset/text_in_Video/ch3_test/'
+        
+        allfiles = list_vids(icdar_loc)
+        random.shuffle(allfiles)
+        
+        data = []
+        
+        for video_name in allfiles:
+            ann_file = icdar_loc+video_name[:-4]+'_GT.xml'
+            ann = parse_ann(ann_file)
+
+            video_orig = skvideo.io.vread(icdar_loc+video_name)
+            n_frames, h, w, _ = video_orig.shape
+            video = np.zeros((n_frames, out_h, out_w, 3), dtype=np.uint8)
+            mask = np.zeros((n_frames, out_h, out_w, 1), dtype=np.uint8)
             
+            for idx in range(n_frames):
+                video[idx] = resize_and_pad((h, w), video_orig[idx])
+                
+                if idx in ann:
+                    polygons = ann[idx]
+                    if polygons:
+                        frame_mask = create_mask((h, w), list(polygons.values()))
+                        mask_resized = resize_and_pad((h, w), frame_mask)
+                        mask[idx] = np.expand_dims(mask_resized, axis=-1)
+            
+            data.append((video, mask, 'icdar'))
+        
+        return data
             
 if __name__ == "__main__":
     dataloaders = TrainDataLoader()
