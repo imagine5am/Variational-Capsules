d5c9e91d5581eb9c4448cbfcc61d068752dd3cf4
SHIVAM SOOD <ssood@cse.iitb.ac.in>
2021-02-07 12:24:05 +0530

    Minor changes

--- CustomDataset.py
+++ CustomDataset.py
@@ -302,7 +302,7 @@ class CustomDataset (Dataset):
         video_dir = os.path.join('/mnt/data/Rohit/VideoCapsNet/data/RoadText-1K', split_type)
         
         if split_type == 'train':
-            selection_ratio = 0.5
+            selection_ratio = 0.25
         else:
             selection_ratio = 1
         
