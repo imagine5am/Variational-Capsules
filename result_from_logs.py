36540cad237effadf4d41ff98d639bba1bc3ae9d
SHIVAM SOOD <ssood@cse.iitb.ac.in>
2021-03-14 21:35:41 +0530

    Change in result_from_logs.py

--- result_from_logs.py
+++ result_from_logs.py
@@ -1,4 +1,10 @@
 with open('./logs/logs_7') as file:
     for line in file:
-        if line.startswith('Train loss: ') or line.startswith('len('):
+        if line.startswith('Train loss: '):
             print(line, sep='')
+            
+        elif line.startswith('Epoch '):
+            print(line, sep='')
+        
+        elif line.startswith('len('):
+            pass
